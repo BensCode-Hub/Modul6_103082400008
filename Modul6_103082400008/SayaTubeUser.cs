2c4247078eee5389ab79f7b6e08270f6b2c48f98
BensCode <ruben.simarmata2@gmail.com>
2026-04-06 08:58:37 +0700

    commit 2 menambahkan 2 class SayaTubeVideo dan SayaTubeUser

---
+++ Modul6_103082400008/SayaTubeUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+
+namespace Modul6_103082400008
+
+{
+    public class SayaTubeUser
+    {
+        public string Username { get; set; }
+        private List<sayaTubeVideo> uploadedVideos;
+
+        public SayaTubeUser(string username)
+        {
+            this.Username = username;
+            this.uploadedVideos = new List<sayaTubeVideo>();
+             }
+        public int getTotalVideoPlayCount()
+        {
+            int total = 0;
+            foreach (var video in uploadedVideos)
+            {
+                total += video.GetPlayCount();
+            }
+            return total;
+        }
+        public void addVideo(sayaTubeVideo video)
+        {
+            this.uploadedVideos.Add(video);
+        }
+        public void PrintAllVideoPlayCount()
+        {
+            Console.WriteLine($"\nUser : {this.Username}");
+            for (int i = 0; i < uploadedVideos.Count; i++)
+            {
+                {
+                    Console.WriteLine($"Video {i + 1} judul: {uploadedVideos[i].getJudul()}");
+                }
+            }
+
+        }
+    }
+}
+
---
+++ Modul6_103082400008/SayaTubeVideo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class sayaTubeVideo
+{
+    private int id;
+    private string judul;
+    private int playCount;
+
+    public sayaTubeVideo(string judul)
+    {
+        this.judul = judul;
+        Random rnd = new Random();
+        this.id = rnd.Next(10000, 99999);
+        this.playCount = 0;
+
+    }
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+
+    }
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"Id Video : {this.id}");
+        Console.WriteLine($"judul Video : {this.judul}");
+        Console.WriteLine($"Play Count : {this.playCount}");
+    }
+    public int GetPlayCount ()
+    {
+        return this.playCount;
+    }
+    public string getJudul()
+    {
+        return this.judul;
+    }
+
+}
